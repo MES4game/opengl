fcaf22cd6a14e3c0f3e72caaef3e8b5f42aba1b0
Maxime DAUPHIN <mes4.game@gmail.com>
2025-04-13 23:20:41 +0200

    added my own skeleton for human

--- main.py
+++ main.py
@@ -82,7 +82,218 @@ def initWork(
     """
     renderer.start = time.perf_counter_ns()
 
-    renderer.scene.addElements(human=src.shapes.Shape(shader_name="scene", mesh_name="human"))
+    human_node = src.shapes.Node()
+
+    # Torse et Tête
+
+    torse = src.shapes.Node(shader_name="scene", mesh_name="cylinder")
+    torse.setCoord(
+        pos=pyrr.Vector3([0.0, 0.0, 0.0], dtype=numpy.single),
+        rot=pyrr.Vector3([0.0, 0.0, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([src.utils.PLAYER_SIZE * 2 / 3, src.utils.PLAYER_SIZE, src.utils.PLAYER_SIZE * 5 / 12], dtype=numpy.single)
+    )
+    human_node.addElements(torse=torse)
+
+    tete = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    tete.setCoord(
+        pos=pyrr.Vector3([0.0, 1.425, 0.0], dtype=numpy.single),
+        rot=pyrr.Vector3([0.0, 0.0, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.6, 0.5, 0.7], dtype=numpy.single)
+    )
+    torse.addElements(tete=tete)
+
+    cou = src.shapes.Node(shader_name="scene", mesh_name="cylinder")
+    cou.setCoord(
+        pos=pyrr.Vector3([0.0, 0.9, 0.0], dtype=numpy.single),
+        rot=pyrr.Vector3([0.0, 0.0, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.3, 0.3, 0.5], dtype=numpy.single)
+    )
+    torse.addElements(cou=cou)
+
+    bassin = src.shapes.Node(shader_name="scene", mesh_name="cylinder")
+    bassin.setCoord(
+        pos=pyrr.Vector3([0.0, -0.75, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([1.1, 0.2, 1.1], dtype=numpy.single)
+    )
+    torse.addElements(bassin=bassin)
+
+    # Bras Gauche
+
+    epaule_gauche = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    epaule_gauche.setCoord(
+        pos=pyrr.Vector3([-0.75, 0.75, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.4, 0.25, 0.7], dtype=numpy.single)
+    )
+    torse.addElements(epaule_gauche=epaule_gauche)
+
+    bras_haut_gauche = src.shapes.Node(shader_name="scene", mesh_name="cylinder")
+    bras_haut_gauche.setCoord(
+        pos=pyrr.Vector3([0.0, -0.45, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.6, 1.5, 0.6], dtype=numpy.single)
+    )
+    epaule_gauche.addElements(bras_haut_gauche=bras_haut_gauche)
+
+    coude_gauche = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    coude_gauche.setCoord(
+        pos=pyrr.Vector3([0.0, -0.3, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([1.1, 0.4, 1.1], dtype=numpy.single)
+    )
+    bras_haut_gauche.addElements(coude_gauche=coude_gauche)
+
+    bras_bas_gauche = src.shapes.Node(shader_name="scene", mesh_name="cylinder")
+    bras_bas_gauche.setCoord(
+        pos=pyrr.Vector3([0.0, -0.3, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.9, 2.5, 0.9], dtype=numpy.single)
+    )
+    coude_gauche.addElements(bras_bas_gauche=bras_bas_gauche)
+
+    main_gauche = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    main_gauche.setCoord(
+        pos=pyrr.Vector3([0.0, -0.3, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.75, 0.75, 0.75], dtype=numpy.single)
+    )
+    bras_bas_gauche.addElements(main_gauche=main_gauche)
+
+    # Bras Droit
+
+    epaule_droite = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    epaule_droite.setCoord(
+        pos=pyrr.Vector3([0.75, 0.75, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.4, 0.25, 0.7], dtype=numpy.single)
+    )
+    torse.addElements(epaule_droite=epaule_droite)
+
+    bras_haut_droit = src.shapes.Node(shader_name="scene", mesh_name="cylinder")
+    bras_haut_droit.setCoord(
+        pos=pyrr.Vector3([0.0, -0.45, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.6, 1.5, 0.6], dtype=numpy.single)
+    )
+    epaule_droite.addElements(bras_haut_droit=bras_haut_droit)
+
+    coude_droit = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    coude_droit.setCoord(
+        pos=pyrr.Vector3([0.0, -0.3, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([1.1, 0.4, 1.1], dtype=numpy.single)
+    )
+    bras_haut_droit.addElements(coude_droit=coude_droit)
+
+    bras_bas_droit = src.shapes.Node(shader_name="scene", mesh_name="cylinder")
+    bras_bas_droit.setCoord(
+        pos=pyrr.Vector3([0.0, -0.3, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.9, 2.5, 0.9], dtype=numpy.single)
+    )
+    coude_droit.addElements(bras_bas_droit=bras_bas_droit)
+
+    main_droite = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    main_droite.setCoord(
+        pos=pyrr.Vector3([0.0, -0.3, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.75, 0.75, 0.75], dtype=numpy.single)
+    )
+    bras_bas_droit.addElements(main_droite=main_droite)
+
+    # Jambe Gauche
+
+    hanche_gauche = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    hanche_gauche.setCoord(
+        pos=pyrr.Vector3([-0.45, -0.15, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.4, 1.5, 0.8], dtype=numpy.single)
+    )
+    bassin.addElements(hanche_gauche=hanche_gauche)
+
+    cuisse_gauche = src.shapes.Node(shader_name="scene", mesh_name="cylinder")
+    cuisse_gauche.setCoord(
+        pos=pyrr.Vector3([0.0, -0.6, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.7, 1.6, 0.6], dtype=numpy.single)
+    )
+    hanche_gauche.addElements(cuisse_gauche=cuisse_gauche)
+
+    genou_gauche = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    genou_gauche.setCoord(
+        pos=pyrr.Vector3([0.0, -0.45, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([1.1, 0.5, 1.1], dtype=numpy.single)
+    )
+    cuisse_gauche.addElements(genou_gauche=genou_gauche)
+
+    jambe_gauche = src.shapes.Node(shader_name="scene", mesh_name="cylinder")
+    jambe_gauche.setCoord(
+        pos=pyrr.Vector3([0.0, -0.45, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.95, 2.0, 0.95], dtype=numpy.single)
+    )
+    genou_gauche.addElements(jambe_gauche=jambe_gauche)
+
+    cheville_gauche = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    cheville_gauche.setCoord(
+        pos=pyrr.Vector3([0.0, -0.45, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([1.1, 0.3, 1.1], dtype=numpy.single)
+    )
+    jambe_gauche.addElements(cheville_gauche=cheville_gauche)
+
+    pied_gauche = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    pied_gauche.setCoord(
+        pos=pyrr.Vector3([0.0, -0.15, 0.15], dtype=numpy.single),
+        size=pyrr.Vector3([1.0, 1.0, 1.5], dtype=numpy.single)
+    )
+    cheville_gauche.addElements(pied_gauche=pied_gauche)
+
+    # Jambe Droite
+
+    hanche_droite = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    hanche_droite.setCoord(
+        pos=pyrr.Vector3([0.45, -0.15, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.4, 1.5, 0.8], dtype=numpy.single)
+    )
+    bassin.addElements(hanche_droite=hanche_droite)
+
+    cuisse_droite = src.shapes.Node(shader_name="scene", mesh_name="cylinder")
+    cuisse_droite.setCoord(
+        pos=pyrr.Vector3([0.0, -0.6, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.7, 1.6, 0.6], dtype=numpy.single)
+    )
+    hanche_droite.addElements(cuisse_droite=cuisse_droite)
+
+    genou_droit = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    genou_droit.setCoord(
+        pos=pyrr.Vector3([0.0, -0.45, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([1.1, 0.5, 1.1], dtype=numpy.single)
+    )
+    cuisse_droite.addElements(genou_droit=genou_droit)
+
+    jambe_droite = src.shapes.Node(shader_name="scene", mesh_name="cylinder")
+    jambe_droite.setCoord(
+        pos=pyrr.Vector3([0.0, -0.45, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([0.95, 2.0, 0.95], dtype=numpy.single)
+    )
+    genou_droit.addElements(jambe_droite=jambe_droite)
+
+    cheville_droite = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    cheville_droite.setCoord(
+        pos=pyrr.Vector3([0.0, -0.45, 0.0], dtype=numpy.single),
+        size=pyrr.Vector3([1.1, 0.3, 1.1], dtype=numpy.single)
+    )
+    jambe_droite.addElements(cheville_droite=cheville_droite)
+
+    pied_droit = src.shapes.Node(shader_name="scene", mesh_name="sphere")
+    pied_droit.setCoord(
+        pos=pyrr.Vector3([0.0, -0.15, 0.15], dtype=numpy.single),
+        size=pyrr.Vector3([1.0, 1.0, 1.5], dtype=numpy.single)
+    )
+    cheville_droite.addElements(pied_droit=pied_droit)
+
+    # Rotation des bras
+    epaule_gauche.rotate(pyrr.Vector3([0.0, -numpy.pi / 2, 0.0], dtype=numpy.single))
+    epaule_droite.rotate(pyrr.Vector3([0.0, numpy.pi / 2, 0.0], dtype=numpy.single))
+
+    # Humain via .obj file
+    human_shape = src.shapes.Shape(shader_name="scene", mesh_name="human")
+
+    human_shape.move(pyrr.Vector3([0.0, -0.5, 2.5], dtype=numpy.single))
+    human_shape.rotate(pyrr.Vector3([0.0, 0.0, numpy.pi], dtype=numpy.single))
+    human_shape.scale(pyrr.Vector3([src.utils.PLAYER_SIZE * 0.9] * 3, dtype=numpy.single))
+    human_node.move(pyrr.Vector3([0.0, 0.0, -2.5], dtype=numpy.single))
+
+    humans = src.shapes.Node()
+    humans.addElements(human_shape=human_shape, human_node=human_node)
+    renderer.scene.addElements(humans=humans)
 
 
 def loopWork(
@@ -98,7 +309,8 @@ def loopWork(
         renderer (src.Renderer): The renderer instance.
         delta_time (float): The elapsed time since the last frame (in second).
     """
-    renderer.scene.elements["human"].rotate(pyrr.Vector3([0.0, 0.0, delta_time], dtype=numpy.single))
+    # TODO
+    pass
 
 
 def main() -> None:
